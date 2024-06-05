6092a775517ae622d0fe62984cab63f4b09b8f58
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2024-06-05 12:42:51 -0500

    Add files via upload

---
+++ ShortcutRecovery.cmd
@@ -0,0 +1,28 @@
+@echo off
+set oldcd=%cd%
+%SystemDrive%
+if exist %SystemDrive%\Users\. goto vista
+if exist "%SystemDrive%\Documents And Settings\." goto xp
+echo Couldn't find start menu folder. This batch file runs on Vista or higher
+goto end
+:xp
+set startMenuUser=\Start Menu
+set startMenuAll=%ALLUSERSPROFILE%\Desktop
+cd \Documents And Settings
+goto begin
+:vista
+set startMenuUser=\APPDATA\Roaming\Microsoft\Windows\Start Menu
+set startMenuAll=%ProgramData%\Microsoft\Windows\Start Menu\Programs
+cd \Users
+:begin
+set smgroup=My Shortcuts
+set /P smgroup=Enter Startmenu Group[%smgroup%]:
+if "%smgroup%"=="" set smgroup=My Shortcuts
+echo Creating folders...
+if not exist "%StartMenuAll%\%SMGroup%\." md "%StartMenuAll%\%SMGroup%"
+for /D %%u in (*) do if not exist "%StartMenuAll%\%SMGroup%\%%u" md "%StartMenuAll%\%SMGroup%\%%u"
+echo Copying Shortcuts...
+for /D %%u in (*) do robocopy "%%u%startMenuUser%" "%StartMenuAll%\%SMGroup%\%%u" /MIR
+:end
+pause
+cd %oldcd%
